a30600bdf36ed99435937b920fadd653832b5fa8
Belmin Fernandez <belminf@gmail.com>
2017-12-31 16:12:37 -0500

    made env vars shorter and added notify

--- magnet-open.sh
+++ magnet-open.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
 # Environment variables needed:
-: "${TRANSMISSION_RPC_URL:?Need to set RPC URL (http://<ip>:<port>/transmission/rpc)}"
-: "${TRANSMISSION_USER:?Need to set RPC username}"
-: "${TRANSMISSION_PASS:?Need to set RPC password}"
-: "${TRANSMISSION_DL_DIR:?Need to set download directory}"
+: "${TR_HOST:=localhost:9091}"
+: "${TR_USER:=transmission}"
+: "${TR_PASS:?Need to set RPC password}"
+: "${TR_DL_DIR:?Need to set download directory}"
 
 # Make sure magnet link sent
 : ${1?"Usage: $0 <magnet link>"}
 MAGNET_LINK="$1"
 
 # Get session ID
-SESSION_ID=$(curl --silent --user "${TRANSMISSION_USER}:${TRANSMISSION_PASS}" "${TRANSMISSION_RPC_URL}" | sed 's/.*\(X-Transmission-Session-Id: [[:alnum:]]\+\).*$/\1/')
+SESSION_ID=$(curl --silent --user "${TR_USER}:${TR_PASS}" "http://${TR_HOST}/transmission/rpc" | sed 's/.*\(X-Transmission-Session-Id: [[:alnum:]]\+\).*$/\1/')
 
 # Submit Magnet link
-curl --silent -H "${SESSION_ID}" "${TRANSMISSION_RPC_URL}" -d '{"method":"torrent-add","arguments":{"paused":false,"download-dir":"'${TRANSMISSION_DL_DIR}'","filename":"'${MAGNET_LINK}'"}}'
+RETURN_JSON=$(curl --silent -H "${SESSION_ID}" "http://${TR_HOST}/transmission/rpc" -d '{"method":"torrent-add","arguments":{"paused":false,"download-dir":"'${TR_DL_DIR}'","filename":"'${MAGNET_LINK}'"}}')
 
+# Notify
+notify-send --app-name="Transmission" "RPC call" "$(echo "${RETURN_JSON}" | sed 's/{"arguments":{"\([^"]\+\)".*"name":"\([^"]\+\)".*"result":"\([^"]\+\)".*$/\1 (\3): \2/')"
